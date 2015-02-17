a187060bd6ec80a5956d50c2c5a51fa6f976cb13
Team293 <293.First@gmail.com>
2015-02-16 19:12:18 -0500

    :circus_tent: Navruz
    LED logic

--- src/SpikeLibrary/SpikeLEDButton.java
+++ src/SpikeLibrary/SpikeLEDButton.java
@@ -5,14 +5,15 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class SpikeLEDButton extends SpikeButton{
 
-	private int outputNumber;
 	private Timer timer = new Timer();
+	private int outputNumber;
 	private double periodLength = 0.25; //length of flashes in seconds
 	private boolean flashState = false;
 	
-	public SpikeLEDButton(Joystick joystickInput, int buttonNumber, int outputNumber) {
-		super(joystickInput, buttonNumber);
-		joystick = joystickInput;
+	public SpikeLEDButton(Joystick joystick, int buttonNumber, int outputNumber) {
+		super(joystick, buttonNumber);
+		this.joystick = joystick;
+		this.outputNumber = outputNumber;
 		timer.start();
 
 	}
